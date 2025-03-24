8b13912099a0afc94d8cf92ce281917317765397
xhuriken <celestin@honvault.Com>
2025-03-24 18:54:56 +0100

    Add Text section, more css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
-const sections = document.querySelectorAll('.project-section');
+const videoSections = document.querySelectorAll('.project-section');
+const textSections = document.querySelectorAll('.text-section');
 const navLinks = document.querySelectorAll('nav a');
 let currentSection = 0;
 
-// display active section
 function showSection(index) {
-  sections.forEach((section, i) => {
+  // update Video Section
+  videoSections.forEach((section, i) => {
     section.classList.toggle('active', i === index);
   });
-  //update link in nav
+  // Update Text Section
+  textSections.forEach((section, i) => {
+    section.classList.toggle('active', i === index);
+  });
+  // Update Nav Links
   navLinks.forEach((link, i) => {
     link.classList.toggle('active-link', i === index);
   });
@@ -23,34 +28,32 @@ navLinks.forEach((anchor, i) => {
 });
 
 // arrow navigation
-sections.forEach((section, i) => {
+videoSections.forEach((section, i) => {
   const leftArrow = section.querySelector('.left-arrow');
   const rightArrow = section.querySelector('.right-arrow');
 
   leftArrow.addEventListener('click', () => {
-    currentSection = (currentSection - 1 + sections.length) % sections.length;
+    currentSection = (currentSection - 1 + videoSections.length) % videoSections.length;
     showSection(currentSection);
   });
 
   rightArrow.addEventListener('click', () => {
-    currentSection = (currentSection + 1) % sections.length;
+    currentSection = (currentSection + 1) % videoSections.length;
     showSection(currentSection);
   });
 });
 
 // mute btn
-sections.forEach(section => {
-    const video = section.querySelector('.video-container video');
-    const muteBtn = section.querySelector('.mute-btn');
-    muteBtn.addEventListener('click', () => {
-      video.muted = !video.muted;
-      if (video.muted) {
-        muteBtn.innerHTML = '<i class="fa-solid fa-volume-mute"></i>';
-      } else {
-        muteBtn.innerHTML = '<i class="fa-solid fa-volume-up"></i>';
-      }
-    });
+videoSections.forEach(section => {
+  const video = section.querySelector('.video-container video');
+  const muteBtn = section.querySelector('.mute-btn');
+  muteBtn.addEventListener('click', () => {
+    video.muted = !video.muted;
+    muteBtn.innerHTML = video.muted 
+      ? '<i class="fa-solid fa-volume-mute"></i>' 
+      : '<i class="fa-solid fa-volume-up"></i>';
   });
+});
 
-// display first section
+// show first section
 showSection(currentSection);
